c44a020c569fa5d6a194143ffc33edd8e5ad0278
Daniel B Kim <danielkim@Daniels-MacBook-Pro.local>
2021-03-18 17:56:14 -0400

    adds most recent changes

--- src/App.js
+++ src/App.js
@@ -8,20 +8,31 @@ import "./App.css";
 function App() {
 
   const [ image, setImage ] = useState([]);
+  // header usestate
+
+
   // use useEffeect hook to grab the GET request from NASA
   useEffect(() => {
     axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
       .then((response) => {
         console.log(response.data);
         setImage(response.data);
+        // set header
       })
       .catch((err) => {
         console.log(err);
       })
   }, []);
 
+  // const style = style.div.a`
+    // styling inline?
+    // not sure how to attach it to each component or HTML element
+
   return (
     <div className="App">
+      <div>
+        <img src= { image.url }></img>
+      </div>
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
