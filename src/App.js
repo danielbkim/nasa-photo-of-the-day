6f3275331444cd2430b66d21bf0d936490a594b3
Daniel B Kim <danielkim@Daniels-MacBook-Pro.local>
2021-03-17 20:03:52 -0400

    adds API call to NASA

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import "./App.css";
 
+// importing some form of an API key using the documentation within NASA
+// https://api.nasa.gov/planetary/apod
+
 function App() {
+
+  const [ image, setImage ] = useState([]);
+  // use useEffeect hook to grab the GET request from NASA
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+      .then((response) => {
+        console.log(response.data);
+        setImage(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }, []);
+
   return (
     <div className="App">
       <p>
